a106e8a5b6efacbffd9b28ce3ecdb732897b8e95
w0nder <154417771+w0nder-official@users.noreply.github.com>
2025-03-12 17:01:49 +0900

    override option 처리 추가

--- cli.js
+++ cli.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 const spawn = require('cross-spawn');
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    o: 'override',
+    p: 'path',
+    h: 'help'
+  }
+});
 const dotenv = require('dotenv-flow');
 const dotenvExpand = require('dotenv-expand');
 
@@ -8,8 +14,10 @@ function printHelp() {
   console.log(
     [
       'Usage: dotenv-flow [-o <override>] [-p <path>] [-- command]',
-      '  -p <path>     Path where `dotenv-flow` should read `.env*` files from. Defaults to current working directory.',
-      '  command       Command to run with ENV vars. Put `--` before the command if it takes additional arguments, otherwise they might get lost.',
+      '  -p, --path <path>        Path where `dotenv-flow` should read `.env*` files from. Defaults to current working directory.',
+      '  -o --override <override> Path to override file that will take precedence over other environment files. Use this for local overrides.',
+      '  -h, --help               Show this help message',
+      '  command                  Command to run with ENV vars. Put `--` before the command if it takes additional arguments, otherwise they might get lost.',
     ].join('\n')
   );
 }
@@ -20,7 +28,8 @@ if (argv.help) {
 }
 
 const path = argv.p;
-dotenvExpand.expand(dotenv.config({ path, override: false }));
+const override = argv.o;
+dotenvExpand.expand(dotenv.config({ path, override }));
 
 const command = argv._[0];
 if (!command) {
