3d695321a4504243dadcc01362b4bffa59d23068
w0nder <154417771+w0nder-official@users.noreply.github.com>
2025-03-12 17:27:46 +0900

    env 주입으로 변경
    .env 가 최우선이게 하려고 했는데, 많은 부분을 바꿔야 함
    강제로 files 통해서 순서를 바꾸고 env 를 주입하도록 변경

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 const spawn = require('cross-spawn');
 const argv = require('minimist')(process.argv.slice(2), {
   alias: {
-    o: 'override',
+    e: 'env',
     p: 'path',
     h: 'help'
   }
@@ -13,9 +13,9 @@ const dotenvExpand = require('dotenv-expand');
 function printHelp() {
   console.log(
     [
-      'Usage: dotenv-flow [-o <override>] [-p <path>] [-- command]',
+      'Usage: dotenv-flow [-e <env>] [-p <path>] [-- command]',
+      '  -e, --env <env>          Environment to use. Defaults to "local".',
       '  -p, --path <path>        Path where `dotenv-flow` should read `.env*` files from. Defaults to current working directory.',
-      '  -o --override <override> Path to override file that will take precedence over other environment files. Use this for local overrides.',
       '  -h, --help               Show this help message',
       '  command                  Command to run with ENV vars. Put `--` before the command if it takes additional arguments, otherwise they might get lost.',
     ].join('\n')
@@ -28,8 +28,13 @@ if (argv.help) {
 }
 
 const path = argv.p;
-const override = argv.o;
-dotenvExpand.expand(dotenv.config({ path, override }));
+const env = argv.e;
+dotenvExpand.expand(dotenv.config({
+  path, files: [
+    `.env.${env}`,
+    '.env'
+  ]
+}));
 
 const command = argv._[0];
 if (!command) {
